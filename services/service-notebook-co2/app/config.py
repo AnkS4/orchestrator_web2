d15ddb43e267e143c4e343e7c6ab7d5b3199793d
Aniket Bharatrao Satbhai <aniket.bharatrao.satbhai@upc.edu>
2025-06-11 10:55:04 +0200

    Revert "Add service files"
    This reverts commit 7cf9a348a1d30afcb04a11810d059ed615c283ab.

--- services/service-notebook-co2/app/config.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-class ServiceConfig:
-    input_file: Optional[str] = None
-    output_file: Optional[str] = None
-    is_running: bool = False
-    progress: int = 0
-    logs: list = []
-    progress_logs: list = []
